f7f742b289183e3a53a6180a8df20776f9a3768f
saddamarbaa <saddamarbaas@gmail.com>
2020-12-14 03:52:57 +0700

    Implementing updateText()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
+ * TODO:  textarea
  * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
@@ -7,11 +7,14 @@
 
 
 /**
- * Update the output text as a user types in the textarea
- * HINT: Use the onkeydown function inside HTML
+ *  function to update the output text as a user types in the textarea
+ * 
  */
+
 function updateText() {
-    // CODE GOES HERE
+    const text = document.getElementById("text-input").value;
+    document.getElementById('text-output').innerText = text;
+    console.log(text);
 }
 
 /**
